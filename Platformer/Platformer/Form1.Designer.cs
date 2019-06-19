00e2d754c9309a3257ba49676383ea14a96d9858
Kaden Adlington <kadenadlington@mmc.school.nz>
2019-06-19 13:24:46 +1200

    Platform Movment

--- Platformer/Platformer/Form1.Designer.cs
+++ Platformer/Platformer/Form1.Designer.cs
@@ -29,8 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer TmrPlatform;
             this.PnlGame = new System.Windows.Forms.Panel();
-            this.TmrPlatform = new System.Windows.Forms.Timer(this.components);
+            this.LblCheck = new System.Windows.Forms.Label();
+            this.LblSpeed1 = new System.Windows.Forms.Label();
+            this.LblSpeed2 = new System.Windows.Forms.Label();
+            this.LblSpeed3 = new System.Windows.Forms.Label();
+            TmrPlatform = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // PnlGame
@@ -42,22 +47,74 @@
             this.PnlGame.TabIndex = 0;
             this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
             // 
+            // TmrPlatform
+            // 
+            TmrPlatform.Enabled = true;
+            TmrPlatform.Tick += new System.EventHandler(this.TmrPlatform_Tick);
+            // 
+            // LblCheck
+            // 
+            this.LblCheck.AutoSize = true;
+            this.LblCheck.Location = new System.Drawing.Point(736, 18);
+            this.LblCheck.Name = "LblCheck";
+            this.LblCheck.Size = new System.Drawing.Size(13, 13);
+            this.LblCheck.TabIndex = 1;
+            this.LblCheck.Text = "0";
+            // 
+            // LblSpeed1
+            // 
+            this.LblSpeed1.AutoSize = true;
+            this.LblSpeed1.Location = new System.Drawing.Point(441, 18);
+            this.LblSpeed1.Name = "LblSpeed1";
+            this.LblSpeed1.Size = new System.Drawing.Size(58, 13);
+            this.LblSpeed1.TabIndex = 2;
+            this.LblSpeed1.Text = "LblSpeed1";
+            // 
+            // LblSpeed2
+            // 
+            this.LblSpeed2.AutoSize = true;
+            this.LblSpeed2.Location = new System.Drawing.Point(505, 18);
+            this.LblSpeed2.Name = "LblSpeed2";
+            this.LblSpeed2.Size = new System.Drawing.Size(58, 13);
+            this.LblSpeed2.TabIndex = 3;
+            this.LblSpeed2.Tag = "";
+            this.LblSpeed2.Text = "LblSpeed2";
+            // 
+            // LblSpeed3
+            // 
+            this.LblSpeed3.AutoSize = true;
+            this.LblSpeed3.Location = new System.Drawing.Point(569, 18);
+            this.LblSpeed3.Name = "LblSpeed3";
+            this.LblSpeed3.Size = new System.Drawing.Size(58, 13);
+            this.LblSpeed3.TabIndex = 4;
+            this.LblSpeed3.Tag = "";
+            this.LblSpeed3.Text = "LblSpeed3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblSpeed3);
+            this.Controls.Add(this.LblSpeed2);
+            this.Controls.Add(this.LblSpeed1);
+            this.Controls.Add(this.LblCheck);
             this.Controls.Add(this.PnlGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel PnlGame;
-        private System.Windows.Forms.Timer TmrPlatform;
+        private System.Windows.Forms.Label LblCheck;
+        private System.Windows.Forms.Label LblSpeed1;
+        private System.Windows.Forms.Label LblSpeed2;
+        private System.Windows.Forms.Label LblSpeed3;
     }
 }
 
--- Platformer/Platformer/Form1.cs
+++ Platformer/Platformer/Form1.cs
@@ -13,9 +13,12 @@ namespace Platformer
     public partial class Form1 : Form
     {
         Graphics g;
-        Rectangle[] area = new Rectangle[3];//area[0] to area[6]
-        Rectangle[] area2 = new Rectangle[3];//area[0] to area[6]
-        Rectangle[] area3 = new Rectangle[3];//area[0] to area[6]
+        Rectangle[] area = new Rectangle[3];//area[0] to area[3]
+        Rectangle[] area2 = new Rectangle[3];//area[0] to area[3]
+        Rectangle[] area3 = new Rectangle[3];//area[0] to area[3]
+        Random speed = new Random();
+        Random speed2 = new Random();
+        Random speed3 = new Random();
         Image platform = Image.FromFile(Application.StartupPath + @"\platform.jpg");
         Image platform2 = Image.FromFile(Application.StartupPath + @"\platform.jpg");
         Image platform3 = Image.FromFile(Application.StartupPath + @"\platform.jpg");
@@ -25,26 +28,52 @@ namespace Platformer
         int gap = 300;
         int gap2 = 300;
         int gap3 = 300;
+        int[] platformSpeed = new int[3];
+        int[] platform2Speed = new int[3];
+        int[] platform3Speed = new int[3];
+        int check = 0;
+
+        private void TmrPlatform_Tick(object sender, EventArgs e)
+        {
+            check = check + 1;
+            LblCheck.Text = check.ToString();
+            for (int i = 0; i <= 2; i++)
+            {
+                area[i].X -= platformSpeed[i];
+            }
+            for (int o = 0; o <= 2; o++)
+            {
+                area2[o].X -= platform2Speed[o];
+            }
+            for (int p = 0; p <= 2; p++)
+            {
+                area3[p].X -= platform3Speed[p];
+            }
+            PnlGame.Invalidate();
+        }
 
         public Form1()
         {
             Random rndgap = new Random();
-            int gap = rndgap.Next(150, 300);
+            int gap = rndgap.Next(300, 300);
             Random rndgap2 = new Random();
-            int gap2 = rndgap2.Next(150, 300);
+            int gap2 = rndgap2.Next(300, 300);
             Random rndgap3 = new Random();
-            int gap3 = rndgap3.Next(150, 300);
+            int gap3 = rndgap3.Next(300, 300);
             for (int i = 0; i <= 2; i++)
             {
+                platformSpeed[i] = speed.Next(10, 20); //each platform has a random speed
                 area[i] = new Rectangle(x1 + gap * i, y, 150, 40);
             }
-            for (int i = 0; i <= 2; i++)
+            for (int o = 0; o <= 2; o++)
             {
-                area2[i] = new Rectangle(x2 + gap2 * i, y2, 150, 40);
+                platform2Speed[o] = speed2.Next(10, 20); //each platform has a random speed
+                area2[o] = new Rectangle(x2 + gap2 * o, y2, 150, 40);
             }
-            for (int i = 0; i <= 2; i++)
+            for (int p = 0; p <= 2; p++)
             {
-                area3[i] = new Rectangle(x3 + gap3 * i, y3, 150, 40);
+                platform3Speed[p] = speed3.Next(10, 20);
+                area3[p] = new Rectangle(x3 + gap3 * p, y3, 150, 40);
             }
             InitializeComponent();
         }
@@ -53,19 +82,30 @@ namespace Platformer
         {
             //get the methods from the graphic's class to paint on the panel
             g = e.Graphics;
-            //use the DrawImage method to draw the spaceship on the panel
-            //use the DrawImage method to draw the planet on the panel
+            //use the DrawImage method to draw the platform on the panel
             for (int i = 0; i <= 2; i++)
             {
                 g.DrawImage(platform, area[i]);
             }
-            for (int i = 0; i <= 2; i++)
+            for (int o = 0; o <= 2; o++)
             {
-                g.DrawImage(platform2, area2[i]);
+                g.DrawImage(platform2, area2[o]);
             }
             for (int i = 0; i <= 2; i++)
             {
                 g.DrawImage(platform3, area3[i]);
+            }
+            for (int i = 0; i <= 2; i++)
+            {
+                platformSpeed[i] = speed.Next(10, 20); //each platform has a random speed
+            }
+            for (int o = 0; o <= 2; o++)
+            {
+                platform2Speed[o] = speed2.Next(10, 20); //each platform has a random speed
+            }
+            for (int p = 0; p <= 2; p++)
+            {
+                platform3Speed[p] = speed2.Next(10, 20); //each platform has a random speed
             }
         }
     }
